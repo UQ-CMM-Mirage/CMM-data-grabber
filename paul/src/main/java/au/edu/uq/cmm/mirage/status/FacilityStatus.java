618f68fc15ae8aa4eff3df6ee7ddb787614359ef
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-02 15:21:25 +1000

    Initial commit

---
+++ paul/src/main/java/au/edu/uq/cmm/mirage/status/FacilityStatus.java
@@ -0,0 +1,63 @@
+package au.edu.uq.cmm.mirage.status;
+
+import java.util.Date;
+
+import au.edu.uq.cmm.aclslib.server.Facility;
+
+public class FacilityStatus {
+    private Facility facility;
+    private String user;
+    private String account;
+    private Date lastLogin;
+    private Date lastLogout;
+    private boolean inUse;
+    
+    public FacilityStatus(Facility facility) {
+        super();
+        this.facility = facility;
+    }
+    
+    public String getUser() {
+        return user;
+    }
+    
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    public String getAccount() {
+        return account;
+    }
+    
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    
+    public Facility getFacility() {
+        return facility;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Date getLastLogout() {
+        return lastLogout;
+    }
+
+    public void setLastLogout(Date lastLogout) {
+        this.lastLogout = lastLogout;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+}
---
+++ paul/src/main/java/au/edu/uq/cmm/mirage/status/FacilityStatusManager.java
@@ -0,0 +1,47 @@
+package au.edu.uq.cmm.mirage.status;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import au.edu.uq.cmm.aclslib.proxy.AclsFacilityEvent;
+import au.edu.uq.cmm.aclslib.proxy.AclsFacilityEventListener;
+import au.edu.uq.cmm.aclslib.proxy.AclsLoginEvent;
+import au.edu.uq.cmm.aclslib.proxy.AclsLogoutEvent;
+import au.edu.uq.cmm.aclslib.proxy.AclsProxy;
+import au.edu.uq.cmm.aclslib.server.Facility;
+
+public class FacilityStatusManager implements AclsFacilityEventListener {
+    // FIXME - the facility statuses need to be persisted.
+    private AclsProxy proxy;
+    private Map<String, FacilityStatus> facilities = 
+            new HashMap<String, FacilityStatus>();
+
+    public FacilityStatusManager(AclsProxy proxy) {
+        this.proxy = proxy;
+        this.proxy.addListener(this);
+    }
+
+    public void eventOccurred(AclsFacilityEvent event) {
+        synchronized (facilities) {
+            Facility facility = event.getFacility();
+            String facilityId = facility.getFacilityId();
+            FacilityStatus status = facilities.get(facilityId);
+            if (status == null) {
+                status = new FacilityStatus(event.getFacility());
+                facilities.put(facilityId, status);
+            }
+            if (event instanceof AclsLoginEvent) {
+                status.setUser(event.getUserName());
+                status.setAccount(event.getAccount());
+                status.setLastLogin(new Date());
+                status.setInUse(true);
+            } else if (event instanceof AclsLogoutEvent) {
+                status.setUser(event.getUserName());
+                status.setAccount(event.getAccount());
+                status.setLastLogout(new Date());
+                status.setInUse(false);
+            }
+        }
+    }
+}
