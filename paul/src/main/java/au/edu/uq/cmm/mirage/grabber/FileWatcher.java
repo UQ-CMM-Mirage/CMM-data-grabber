0b1ab1189bb9524a0257034966865cd8e430992e
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-06 17:19:22 +1000

    Apply the startup / shutdown stuff here too

---
+++ paul/src/main/java/au/edu/uq/cmm/mirage/grabber/FileWatcher.java
@@ -0,0 +1,64 @@
+package au.edu.uq.cmm.mirage.grabber;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.WatchEvent.Kind;
+
+import org.apache.log4j.Logger;
+
+import au.edu.uq.cmm.aclslib.service.MonitoredThreadServiceBase;
+
+public class FileWatcher extends MonitoredThreadServiceBase {
+    private static final Logger LOG = Logger.getLogger(FileWatcher.class);
+    
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
+    }
+
+    @Override
+    public void run() {
+        try {
+            FileSystem fs = FileSystems.getDefault();
+            WatchService watcher = fs.newWatchService();
+            Path dir = Paths.get(new File("/tmp").toURI());
+            dir.register(watcher, 
+                    StandardWatchEventKinds.ENTRY_CREATE, 
+                    StandardWatchEventKinds.ENTRY_MODIFY, 
+                    StandardWatchEventKinds.ENTRY_DELETE, 
+                    StandardWatchEventKinds.OVERFLOW);
+            while (true) {
+                WatchKey key = watcher.take();
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    Kind<?> kind = event.kind();
+                    if (kind == StandardWatchEventKinds.OVERFLOW) {
+                        LOG.error("Event overflow!");
+                        continue;
+                    }
+                    WatchEvent<Path> ev = cast(event);
+                    Path file = dir.resolve(ev.context());
+                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                        System.err.println("Created - " + file);
+                    } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        System.err.println("Modified - " + file);
+                    } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                        System.err.println("Deleted - " + file);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            throw new GrabberException("Unexpected IO error", ex);
+        } catch (InterruptedException ex) {
+            LOG.debug("Interrupted ... we're done");
+        }
+    }
+
+}
--- paul/src/main/java/au/edu/uq/cmm/mirage/grabber/Grabber.java
+++ paul/src/main/java/au/edu/uq/cmm/mirage/grabber/Grabber.java
@@ -1,29 +1,18 @@
 package au.edu.uq.cmm.mirage.grabber;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchEvent.Kind;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-
 import org.apache.log4j.Logger;
 
 import au.edu.uq.cmm.aclslib.proxy.AclsProxy;
 import au.edu.uq.cmm.aclslib.server.Configuration;
+import au.edu.uq.cmm.aclslib.service.CompositeServiceBase;
+import au.edu.uq.cmm.aclslib.service.ServiceException;
 import au.edu.uq.cmm.mirage.status.FacilityStatusManager;
 
-public class Grabber implements Runnable {
+public class Grabber extends CompositeServiceBase {
     
     private static final Logger LOG = Logger.getLogger(Grabber.class);
     private Configuration config;
-    private Thread grabberThread;
+    private FileWatcher fileWatcher;
     private FacilityStatusManager statusManager;
     private AclsProxy proxy;
     
@@ -49,7 +38,7 @@ public class Grabber implements Runnable {
             FacilityStatusManager statusManager = new FacilityStatusManager(proxy);
             Grabber grabber = new Grabber(config, proxy, statusManager);
             grabber.startup();
-            grabber.shutdown();
+            grabber.awaitShutdown();
             LOG.info("Exitting normally");
             System.exit(0);
         } catch (Throwable ex) {
@@ -58,88 +47,13 @@ public class Grabber implements Runnable {
         }
     }
     
-    private void shutdown() {
-        LOG.info("Shutting down");
-        try {
-            grabberThread.interrupt();
-            grabberThread.join(5000);
-        } catch (InterruptedException ex) {
-            LOG.debug(ex);
-        }
+    protected void doShutdown() {
+        fileWatcher.shutdown();
         proxy.shutdown();
     }
 
-    private void startup() throws UnknownHostException {
-        LOG.info("Starting up");
+    protected void doStartup() throws ServiceException {
         proxy.startup();
-        grabberThread = launch();
-        LOG.info("Started");
-        try {
-            while (true) {
-                if (!grabberThread.isAlive()) {
-                    LOG.info("Listener thread died");
-                    grabberThread.join();
-                    grabberThread = launch();
-                    LOG.info("Restarted");
-                }
-                Thread.sleep(5000);
-            }
-        } catch (InterruptedException ex) {
-            LOG.debug(ex);
-        }
-    }
-
-    private Thread launch() throws UnknownHostException {
-        Thread thread = new Thread(this);
-        thread.setDaemon(true);
-        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            public void uncaughtException(Thread t, Throwable ex) {
-                LOG.debug(ex);
-            }
-        });
-        thread.start();
-        return thread;
-    }
-    
-    @SuppressWarnings("unchecked")
-    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-        return (WatchEvent<T>)event;
-    }
-
-    @Override
-    public void run() {
-        try {
-            FileSystem fs = FileSystems.getDefault();
-            WatchService watcher = fs.newWatchService();
-            Path dir = Paths.get(new File("/tmp").toURI());
-            dir.register(watcher, 
-                    StandardWatchEventKinds.ENTRY_CREATE, 
-                    StandardWatchEventKinds.ENTRY_MODIFY, 
-                    StandardWatchEventKinds.ENTRY_DELETE, 
-                    StandardWatchEventKinds.OVERFLOW);
-            while (true) {
-                WatchKey key = watcher.take();
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    Kind<?> kind = event.kind();
-                    if (kind == StandardWatchEventKinds.OVERFLOW) {
-                        LOG.error("Event overflow!");
-                        continue;
-                    }
-                    WatchEvent<Path> ev = cast(event);
-                    Path file = dir.resolve(ev.context());
-                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-                        System.err.println("Created - " + file);
-                    } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-                        System.err.println("Modified - " + file);
-                    } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-                        System.err.println("Deleted - " + file);
-                    }
-                }
-            }
-        } catch (IOException ex) {
-            throw new GrabberException("Unexpected IO error", ex);
-        } catch (InterruptedException ex) {
-            LOG.debug("Interrupted ... we're done");
-        }
+        fileWatcher.startup();
     }
 }
