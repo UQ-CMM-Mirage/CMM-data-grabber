249508b051faa1fa35f50ff5d853f27c2dbed787
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-06 14:30:16 +1000

    First attempt at directory watching

--- paul/src/main/java/au/edu/uq/cmm/mirage/grabber/Grabber.java
+++ paul/src/main/java/au/edu/uq/cmm/mirage/grabber/Grabber.java
@@ -8,7 +8,9 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
+import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
 import org.apache.log4j.Logger;
@@ -98,21 +100,46 @@ public class Grabber implements Runnable {
         thread.start();
         return thread;
     }
+    
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
+    }
 
     @Override
     public void run() {
-        
         try {
             FileSystem fs = FileSystems.getDefault();
             WatchService watcher = fs.newWatchService();
-            Path facilityDirectory = Paths.get(new File("/tmp").toURI());
-            facilityDirectory.register(watcher, 
+            Path dir = Paths.get(new File("/tmp").toURI());
+            dir.register(watcher, 
                     StandardWatchEventKinds.ENTRY_CREATE, 
                     StandardWatchEventKinds.ENTRY_MODIFY, 
                     StandardWatchEventKinds.ENTRY_DELETE, 
                     StandardWatchEventKinds.OVERFLOW);
+            while (true) {
+                WatchKey key = watcher.take();
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    Kind<?> kind = event.kind();
+                    if (kind == StandardWatchEventKinds.OVERFLOW) {
+                        LOG.error("Event overflow!");
+                        continue;
+                    }
+                    WatchEvent<Path> ev = cast(event);
+                    Path file = dir.resolve(ev.context());
+                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                        System.err.println("Created - " + file);
+                    } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                        System.err.println("Modified - " + file);
+                    } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                        System.err.println("Deleted - " + file);
+                    }
+                }
+            }
         } catch (IOException ex) {
             throw new GrabberException("Unexpected IO error", ex);
+        } catch (InterruptedException ex) {
+            LOG.debug("Interrupted ... we're done");
         }
     }
 }
