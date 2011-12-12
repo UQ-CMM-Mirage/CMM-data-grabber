c013801e3c224f53e37e24f61c3eb974be0ac16c
Stephen Crawley <s.crawley@uq.edu.au>
2011-12-12 13:31:37 +1000

    Support watching of nested directories

--- paul/src/main/java/au/edu/uq/cmm/mirage/grabber/FileWatcher.java
+++ paul/src/main/java/au/edu/uq/cmm/mirage/grabber/FileWatcher.java
@@ -20,17 +20,16 @@ import org.apache.log4j.Logger;
 import au.edu.uq.cmm.aclslib.server.Configuration;
 import au.edu.uq.cmm.aclslib.server.Facility;
 import au.edu.uq.cmm.aclslib.service.MonitoredThreadServiceBase;
+import au.edu.uq.cmm.aclslib.service.ThreadServiceBase;
 
-public class FileWatcher extends MonitoredThreadServiceBase {
+public class FileWatcher extends ThreadServiceBase {
     private static class WatcherEntry {
         private final Path dir;
-        private final WatchKey key;
         private final Facility facility;
         
-        public WatcherEntry(Path dir, WatchKey key, Facility facility) {
+        public WatcherEntry(Path dir, Facility facility) {
             super();
             this.dir = dir;
-            this.key = key;
             this.facility = facility;
         }
     }
@@ -40,6 +39,7 @@ public class FileWatcher extends MonitoredThreadServiceBase {
     private Configuration config;
     private Map<WatchKey, WatcherEntry> watchMap = new HashMap<WatchKey, WatcherEntry>();
     private UncPathnameMapper uncNameMapper;
+    private WatchService watcher;
     
     public FileWatcher(Configuration config, UncPathnameMapper uncNameMapper) 
             throws UnknownHostException {
@@ -54,9 +54,9 @@ public class FileWatcher extends MonitoredThreadServiceBase {
 
     @Override
     public void run() {
-        WatchService watcher = null;
+        watcher = null;
         try {
-            watcher = configureWatcher();
+            configureWatcher();
             while (true) {
                 WatchKey key = watcher.take();
                 WatcherEntry entry = watchMap.get(key);
@@ -82,27 +82,32 @@ public class FileWatcher extends MonitoredThreadServiceBase {
         }
     }
 
-    private void processWatchEvent(WatcherEntry entry, WatchEvent<?> event) {
+    private void processWatchEvent(WatcherEntry entry, WatchEvent<?> event) 
+            throws IOException {
         Kind<?> kind = event.kind();
         if (kind == StandardWatchEventKinds.OVERFLOW) {
             LOG.error("Event overflow!");
             return;
         } 
         WatchEvent<Path> ev = cast(event);
-        Path file = entry.dir.resolve(ev.context());
+        Path path = entry.dir.resolve(ev.context());
         LOG.debug("Event for facility " + entry.facility.getFacilityId());
         if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-            LOG.debug("Created - " + file);
+            LOG.debug("Created - " + path);
+            File file = path.toFile();
+            if (file.isDirectory()) {
+                addKeys(entry.facility, file);
+            }
         } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-            LOG.debug("Modified - " + file);
+            LOG.debug("Modified - " + path);
         } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-            LOG.debug("Deleted - " + file);
+            LOG.debug("Deleted - " + path);
         }
     }
 
-    private WatchService configureWatcher() throws IOException {
+    private void configureWatcher() throws IOException {
         FileSystem fs = FileSystems.getDefault();
-        WatchService watcher = fs.newWatchService();
+        watcher = fs.newWatchService();
         for (Facility facility : config.getFacilities().values()) {
             if (facility.getDriveName() == null) {
                 continue;
@@ -118,14 +123,30 @@ public class FileWatcher extends MonitoredThreadServiceBase {
                         "' maps to non-existent local path '" + local + "'");
                 continue;
             }
-            Path dir = Paths.get(local.toURI());
-            WatchKey key = dir.register(watcher, 
-                    StandardWatchEventKinds.ENTRY_CREATE, 
-                    StandardWatchEventKinds.ENTRY_MODIFY, 
-                    StandardWatchEventKinds.ENTRY_DELETE, 
-                    StandardWatchEventKinds.OVERFLOW);
-            watchMap.put(key, new WatcherEntry(dir, key, facility));
+            addKeys(facility, local);
         }
-        return watcher;
+    }
+
+    private void addKeys(Facility facility, File local) 
+            throws IOException {
+        // If a directory is created while we are recursively adding
+        // watcher keys, we may possibly miss it.  However, I think 
+        // that we should get an event for the creation ... which would
+        // allow us to add the key in the event processing code.
+        Path dir = Paths.get(local.toURI());
+        WatchKey key = dir.register(watcher, 
+                StandardWatchEventKinds.ENTRY_CREATE, 
+                StandardWatchEventKinds.ENTRY_MODIFY, 
+                StandardWatchEventKinds.ENTRY_DELETE, 
+                StandardWatchEventKinds.OVERFLOW);
+        LOG.debug("Added directory watcher for " + local + 
+                " for facility " + facility);
+        watchMap.put(key, new WatcherEntry(dir, facility));
+        // Recursively add keys for nested directories.
+        for (File child : local.listFiles()) {
+            if (child.isDirectory()) {
+                addKeys(facility, child);
+            }
+        }
     }
 }
