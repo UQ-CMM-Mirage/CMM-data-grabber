9042faa211be50fe1eaef72ec9153390b4af92a0
Stephen Crawley <s.crawley@uq.edu.au>
2012-08-06 16:07:50 +1000

    Finished the unit test for the fix for the slow copy race condition.

--- paul/src/test/java/au/edu/uq/cmm/paul/grabber/GrabberEventTest.java
+++ paul/src/test/java/au/edu/uq/cmm/paul/grabber/GrabberEventTest.java
@@ -27,6 +27,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -88,8 +90,15 @@ public class GrabberEventTest {
         template.setOptional(true);
         template.setMinimumSize(20);
         template.setFilePattern("(.*)\\.txt");
-        List<DatafileTemplate> templates = Collections.singletonList(template);
-        facility.setDatafileTemplates(templates);
+        DatafileTemplate template2 = new DatafileTemplate();
+        template2.setSuffix("tox");
+        template2.setMimeType("text/plain");
+        template2.setOptional(true);
+        template2.setMinimumSize(20);
+        template2.setFilePattern("(.*)\\.tox");
+        facility.setDatafileTemplates(Arrays.asList(new DatafileTemplate[] {
+                template, template2
+        }));
         return facility;
     }
 
@@ -240,28 +249,29 @@ public class GrabberEventTest {
         assertEquals(new Date(now), dataset.getCaptureTimestamp());
     }
     
-
-    
     @Test
     public void testSlowGrabbing() 
             throws InterruptedException, JsonGenerationException, IOException, QueueFileException {
         Capture<DatasetMetadata> capture = new Capture<DatasetMetadata>();
-        QueueFileManager fm = new SlowCopyingQueueFileManager(CONFIG);
+        SlowCopyingQueueFileManager fm = new SlowCopyingQueueFileManager(CONFIG);
         QueueManager qm = EasyMock.createMock(QueueManager.class);
         qm.addEntry(EasyMock.capture(capture));
         EasyMock.expectLastCall().times(1);
         EasyMock.expect(qm.getFileManager()).andReturn(fm).times(1);
         EasyMock.replay(qm);
-        FACILITY.setFileSettlingTime(1000);
+        FACILITY.setFileSettlingTime(50);
         FileGrabber fg = new FileGrabber(buildMockServices(CONFIG, qm), FACILITY, true);
         long now = System.currentTimeMillis();
         try {
             File one = new File("/tmp/one.txt");
+            File onet = new File("/tmp/one.tox");
             stringToFile("123456789012345678901234567890", one);
+            stringToFile("123456789012345678901234567890", onet);
             fg.startup();
             long tmp = now;
             for (int i = 0; i < 10; i++) {
                 fg.eventOccurred(new FileWatcherEvent(FACILITY, one, true, tmp, false));
+                fg.eventOccurred(new FileWatcherEvent(FACILITY, onet, true, tmp, false));
                 tmp = System.currentTimeMillis();
                 Thread.sleep(100);
             }
@@ -272,7 +282,7 @@ public class GrabberEventTest {
         DatasetMetadata dataset = capture.getValue();
         assertEquals("fred", dataset.getUserName());
         assertEquals("count", dataset.getAccountName());
-        assertEquals(1, dataset.getDatafiles().size());
+        assertEquals(2, dataset.getDatafiles().size());
         DatafileMetadata datafile = dataset.getDatafiles().get(0);
         File file = new File(datafile.getCapturedFilePathname());
         assertTrue(file.exists());
@@ -281,6 +291,7 @@ public class GrabberEventTest {
                      "e56f0eef73ade8f79bc1d16a99cbc5e4995afd8c14adb49410ecd957aecc8d02", 
                      datafile.getDatafileHash().toLowerCase());
         assertEquals(new Date(now), dataset.getCaptureTimestamp());
+        assertEquals(9, fm.getFilesRemoved().size());
     }
 
     private void stringToFile(String str, File file) throws IOException {
@@ -304,6 +315,8 @@ public class GrabberEventTest {
     }
     
     public static class SlowCopyingQueueFileManager extends CopyingQueueFileManager {
+        private List<File> filesRemoved = new ArrayList<File>();
+        
         public SlowCopyingQueueFileManager(PaulConfiguration config) {
             super(config);
         }
@@ -311,8 +324,20 @@ public class GrabberEventTest {
         @Override
         public File enqueueFile(File source, String suffix, boolean regrabbing)
                 throws QueueFileException, InterruptedException {
-            Thread.sleep(1000);
+            if (suffix.equals("tox")) {
+                Thread.sleep(1000);
+            }
             return super.enqueueFile(source, suffix, regrabbing);
+        }
+
+        @Override
+        public void removeFile(File file) throws QueueFileException {
+            filesRemoved.add(file);
+            super.removeFile(file);
+        }
+
+        public final List<File> getFilesRemoved() {
+            return filesRemoved;
         }
     }
 }
