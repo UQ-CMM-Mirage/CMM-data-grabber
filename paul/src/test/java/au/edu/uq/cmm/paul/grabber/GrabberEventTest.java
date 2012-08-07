49f0905ff370afe2cc26e5bd6c8f8fe4d53e6534
Stephen Crawley <s.crawley@uq.edu.au>
2012-08-07 14:47:55 +1000

    Fixed product name in copyright header ...

--- paul/src/test/java/au/edu/uq/cmm/paul/grabber/GrabberEventTest.java
+++ paul/src/test/java/au/edu/uq/cmm/paul/grabber/GrabberEventTest.java
@@ -1,9 +1,9 @@
 /*
 * Copyright 2012, CMM, University of Queensland.
 *
-* This file is part of AclsLib.
+* This file is part of Paul.
 *
-* AclsLib is free software: you can redistribute it and/or modify
+* Paul is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
@@ -14,7 +14,7 @@
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with AclsLib. If not, see <http://www.gnu.org/licenses/>.
+* along with Paul. If not, see <http://www.gnu.org/licenses/>.
 */
 
 package au.edu.uq.cmm.paul.grabber;
