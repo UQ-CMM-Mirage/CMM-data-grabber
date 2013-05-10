9955219eeed9b6ec6b1f72b06f28e47a107bc995
Stephen Crawley <crawley.stephen@gmail.com>
2013-05-10 15:43:40 +1000

    Tweak the database mapping ...

--- paul/src/main/java/au/edu/uq/cmm/paul/status/Facility.java
+++ paul/src/main/java/au/edu/uq/cmm/paul/status/Facility.java
@@ -19,11 +19,14 @@
 
 package au.edu.uq.cmm.paul.status;
 
+import java.lang.annotation.ElementType;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -263,6 +266,7 @@ public class Facility implements FacilityConfig {
         this.status = status;
     }
 
+    @Enumerated(EnumType.STRING)
     public final FileArrivalMode getFileArrivalMode() {
         return fileArrivalMode;
     }
